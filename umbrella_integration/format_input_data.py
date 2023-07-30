ce070298ce6bc6e070bd4d1ff441f7f66e2f4c65
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2023-07-30 15:19:06 +0200

    linting

--- umbrella_integration/format_input_data.py
+++ umbrella_integration/format_input_data.py
@@ -4,19 +4,23 @@
 #
 # Note: assumes the first 2 columns in input_file are white-space separated and of the form:
 # time(ps) reaction_cooridinate_position
-from argparse import ArgumentParser
-from os.path import basename
-import numpy as np
 import glob
 import gzip
 import itertools
 import logging
+from argparse import ArgumentParser
+from os.path import basename
 
-logging.basicConfig(level=logging.INFO, format='[%(levelname)s]: %(message)s')
+import numpy as np
+
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s]: %(message)s")
 
 SKIP_LINE_CHARACTERS = ["#", "@"]
-UI_INPUT_FILE_HEADER = "#  time  position  equilibrium_position  force_constant(kJ mol^-1 distance^-2)"
+UI_INPUT_FILE_HEADER = (
+    "#  time  position  equilibrium_position  force_constant(kJ mol^-1 distance^-2)"
+)
 UI_INPUT_LINE_TEMPLATE = "{0:>10g}   {1:>10g}      {2:>10g}                  {3:>10g}"
+
 
 # Reformat the first two columns of the input_file to be a list of lines of the form:
 # time  position  equilibrium_position  force_constant(kJ mol^-1 distance^-2)
@@ -32,7 +36,11 @@ def generate_ui_input_lines(input_file, eq_pos, fc, transform_from_relative=Fals
     if transform_from_relative:
         pos = pos + eq_pos
     n = len(time)
-    return [UI_INPUT_LINE_TEMPLATE.format(t, p, eq_p, f) for t, p, eq_p, f in zip(time, pos, [eq_pos]*n, [fc]*n)]
+    return [
+        UI_INPUT_LINE_TEMPLATE.format(t, p, eq_p, f)
+        for t, p, eq_p, f in zip(time, pos, [eq_pos] * n, [fc] * n)
+    ]
+
 
 # Reformat and merge data into a single file.
 # run_data needs to be a list of the form:
@@ -44,73 +52,118 @@ def generate_ui_input_lines(input_file, eq_pos, fc, transform_from_relative=Fals
 def generate_ui_input_lines_merged(run_data, transform_from_relative=False):
     ui_input_lines = []
     # sort data on eq_pos first and then input_file
-    for input_file, eq_pos, fc in sorted(run_data, key=lambda x:(x[2], x[0])):
+    for input_file, eq_pos, fc in sorted(run_data, key=lambda x: (x[2], x[0])):
         logging.debug("Processing {0}".format(input_file))
-        ui_input_lines.extend(generate_ui_input_lines(input_file, eq_pos, fc, transform_from_relative))
+        ui_input_lines.extend(
+            generate_ui_input_lines(input_file, eq_pos, fc, transform_from_relative)
+        )
     return ui_input_lines
+
 
 def write_output_file(output_file, ui_input_lines):
     with open(output_file, "w") as fh:
         fh.write("\n".join([UI_INPUT_FILE_HEADER] + ui_input_lines))
 
+
 def parse_input_file(input_file, keep_every=1):
     open_function = gzip.open if input_file.endswith(".gz") else open
     fh = open_function(input_file)
     lines = fh.read().splitlines()
-    #print "Parsing file, keeping every {0}(st/nd/rd/th) value".format(keep_every)
+    # print "Parsing file, keeping every {0}(st/nd/rd/th) value".format(keep_every)
     line_iterator = itertools.islice(lines, None, None, keep_every)
-    lines = np.array([l.split()[:2] for l in line_iterator if l.strip()[0] not in SKIP_LINE_CHARACTERS], dtype=np.float64)
+    lines = np.array(
+        [
+            l.split()[:2]
+            for l in line_iterator
+            if l.strip()[0] not in SKIP_LINE_CHARACTERS
+        ],
+        dtype=np.float64,
+    )
     fh.close()
     return lines.T
 
-def parse_commandline():
 
+def parse_commandline():
     parser = ArgumentParser(prog=basename(__file__))
     # python -i <input data file> -r <reaction coordinate equilibrium position> -f <force constant> [-s]")
     # required
-    parser.add_argument("-i", "--input_file",
+    parser.add_argument(
+        "-i",
+        "--input_file",
         help="Input data file, white space separated columns of the form: col1=time, col2=position",
-        action="store", required=True)
+        action="store",
+        required=True,
+    )
 
-    parser.add_argument("-o", "--output_file",
+    parser.add_argument(
+        "-o",
+        "--output_file",
         help="Output data file: col1=time, col2=position, col5=umbrella_equilibrium_position, col4=force_constant (kJ mol^-1 distance^-2)",
-        action="store", required=True)
+        action="store",
+        required=True,
+    )
 
-    parser.add_argument("-f", "--force_constant",
+    parser.add_argument(
+        "-f",
+        "--force_constant",
         help="Force constant used for the umbrella potential (kJ mol^-1 distance^-2)",
-        action="store", required=True, type=float)
+        action="store",
+        required=True,
+        type=float,
+    )
 
-    parser.add_argument("-r", "--reaction_coordinate_position",
+    parser.add_argument(
+        "-r",
+        "--reaction_coordinate_position",
         help="Equilibrium position of the umbrella potential along the reaction coordinate",
-        action="store", required=True, type=float)
+        action="store",
+        required=True,
+        type=float,
+    )
 
-    parser.add_argument("-s", "--transform_from_relative",
+    parser.add_argument(
+        "-s",
+        "--transform_from_relative",
         help="Shift all reaction coordinate positions by value set for reaction_coordinate_position (helpful since some packages output positions relative to the umbrella equilibrium position)",
-        action="store_true", required=False)
+        action="store_true",
+        required=False,
+    )
 
     args = parser.parse_args()
 
-    return args.input_file, args.output_file, args.force_constant, args.reaction_coordinate_position, args.transform_from_relative
+    return (
+        args.input_file,
+        args.output_file,
+        args.force_constant,
+        args.reaction_coordinate_position,
+        args.transform_from_relative,
+    )
+
 
 def run_umbrella_integration():
     input_file, output_file, fc, eq_pos, transform_from_relative = parse_commandline()
-    ui_input_lines = generate_ui_input_lines(input_file, eq_pos, fc, transform_from_relative)
+    ui_input_lines = generate_ui_input_lines(
+        input_file, eq_pos, fc, transform_from_relative
+    )
     write_output_file(output_file, ui_input_lines)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     run_umbrella_integration()
     # Below is an example of a more advanced use of this module to reformat and merge data from
     # multiple umbrella simulations. Also see examples/example_analysis_script.py
     if False:
         # example script for generating merged umbrella_integration.py input file
         # force constant is the same for all windows
-        fc = 500000.0 # J mol-1 nm-2
-        fc /= 1000.0 # kJ mol-1 nm-2
+        fc = 500000.0  # J mol-1 nm-2
+        fc /= 1000.0  # kJ mol-1 nm-2
         run_data = []
         # use a glob pattern to list relavent files
         for f in glob.glob("Nafc500*.dat"):
             # extract equilibrium position (window_center) from filename
             window_center = float(basename(f).split("_")[3])
-            run_data.append( (f, window_center, fc) )
-        ui_input_lines = generate_ui_input_lines_merged(run_data, transform_from_relative=False)
+            run_data.append((f, window_center, fc))
+        ui_input_lines = generate_ui_input_lines_merged(
+            run_data, transform_from_relative=False
+        )
         write_output_file("na_ion_merged_ui_input.dat", ui_input_lines)
--- umbrella_integration/helpers/plot_data.py
+++ umbrella_integration/helpers/plot_data.py
@@ -1,40 +1,49 @@
-import numpy as np
 import matplotlib
+import numpy as np
+
 matplotlib.use("Agg")
 try:
     from scipy.stats.kde import gaussian_kde
+
     CAN_PLOT_KDE = True
 except ImportError:
-    print ("KDE will be omitted from window distribution plot due to missing scipy library.")
+    print(
+        "KDE will be omitted from window distribution plot due to missing scipy library."
+    )
     CAN_PLOT_KDE = False
 
 import matplotlib.pyplot as plt
+
 matplotlib.rc("mathtext", fontset="stix")
 
-LINE_WIDTH = 1.25 # table borders and ticks
+LINE_WIDTH = 1.25  # table borders and ticks
 MARKER_SIZE = 5
 
 FONT_PARAMS = dict(
     # family='serif',
     # serif='Times New Roman',
     size=12,
-    )
+)
 x_label_size_increase = 4
 y_label_size_increase = 1
 
-matplotlib.rc('font', **FONT_PARAMS)
-matplotlib.rc('axes', linewidth=LINE_WIDTH)
+matplotlib.rc("font", **FONT_PARAMS)
+matplotlib.rc("axes", linewidth=LINE_WIDTH)
 
-def create_figure(figsize=(5,4)):
+
+def create_figure(figsize=(5, 4)):
     fig = matplotlib.pyplot.figure(figsize=figsize)
     fig.hold = True
     return fig
 
+
 def add_axis_to_figure(fig, subplot_layout=111, sharex=None, sharey=None):
     return fig.add_subplot(subplot_layout, sharex=sharex, sharey=sharey)
 
+
 def save_figure(fig, fig_name):
     fig.savefig(fig_name)
+
 
 def simple_plot(xs, ys, es, xlabel, ylabel, fig_name=None, show_auc=False):
     fig = create_figure()
@@ -43,20 +52,51 @@ def simple_plot(xs, ys, es, xlabel, ylabel, fig_name=None, show_auc=False):
 
     finalise_figure(fig, ax, xlabel, ylabel, fig_name)
 
-def plot(ax, xs, ys, es, xlabel=None, ylabel=None, label=None, color="b", linewidth=LINE_WIDTH, alpha=1, line_style="-", show_auc=False):
 
-    ax.errorbar(xs, ys, yerr=es, marker="o", zorder=1, linestyle=line_style, color=color, markerfacecolor=color, label=label, markersize=MARKER_SIZE, alpha=alpha, linewidth=linewidth)
+def plot(
+    ax,
+    xs,
+    ys,
+    es,
+    xlabel=None,
+    ylabel=None,
+    label=None,
+    color="b",
+    linewidth=LINE_WIDTH,
+    alpha=1,
+    line_style="-",
+    show_auc=False,
+):
+    ax.errorbar(
+        xs,
+        ys,
+        yerr=es,
+        marker="o",
+        zorder=1,
+        linestyle=line_style,
+        color=color,
+        markerfacecolor=color,
+        label=label,
+        markersize=MARKER_SIZE,
+        alpha=alpha,
+        linewidth=linewidth,
+    )
     if show_auc:
-        ax.fill_between(xs, ys, np.zeros(len(xs)), facecolor='blue', alpha=0.2)
+        ax.fill_between(xs, ys, np.zeros(len(xs)), facecolor="blue", alpha=0.2)
 
-def plot_with_kde(ax, xs, ys, xlabel=None, ylabel=None, label=None, show=True, kde=None, color="blue"):
+
+def plot_with_kde(
+    ax, xs, ys, xlabel=None, ylabel=None, label=None, show=True, kde=None, color="blue"
+):
     ax.plot(xs, ys, color=color, label=label)
     if kde:
         kde_curve = kde(np.array(xs))
         ax.fill_between(xs, kde_curve, alpha=0.5, facecolor=color)
 
+
 def plot_hist_to_axis(ax, centers, his, kde=None):
     plot_with_kde(ax, centers, his, show=False, kde=kde)
+
 
 def finalise_figure(fig, ax, xlabel, ylabel, fig_name=None, show=False):
     plt.ylabel(ylabel, fontsize=FONT_PARAMS["size"] + y_label_size_increase)
@@ -72,8 +112,9 @@ def finalise_figure(fig, ax, xlabel, ylabel, fig_name=None, show=False):
         plt.show()
     matplotlib.pyplot.close(fig)
 
+
 def generate_histogram(values, n_bins=50):
     his, bins = np.histogram(values, bins=n_bins, density=True)
-    centers = (bins[:-1]+bins[1:])/2
+    centers = (bins[:-1] + bins[1:]) / 2
     kde = gaussian_kde(values) if CAN_PLOT_KDE else None
     return centers, his, kde
